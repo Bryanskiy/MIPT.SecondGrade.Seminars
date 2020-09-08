2d2b0330c93acad009fa1c19eace9249bc9783b0
Bryanskiy <ivakin.ki@phystech.edu>
2020-09-08 17:27:25 +0300

    add first file

---
+++ sem1/rebase.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+enum flags {
+    OK,
+    BAD_ALLOC,
+    WRONG_INPUT,
+};
+
+int _FLAG_ = OK;
+
+enum consts {
+    ALLOC_START = 5,
+    ALLOC_EXPAND = 2,
+};
+
+struct number {
+    unsigned char* digits;
+    unsigned base;
+    unsigned len;
+};
+
+unsigned char char_to_int(char c) {
+    if ( c >= '0' && c <= '9'){
+        return c - '0';
+    }
+
+    else if ( c >= 'A' && c <= 'Z'){
+        return c - 'A' + 10;
+    }
+
+    else if ( c >= 'a' && c <= 'z'){
+        return c - 'a' + 10;
+    }
+}
+
+struct number read_number(FILE* in) {
+    struct number ret = {NULL, 0, 0};
+
+    ret.digits = (char*)calloc(ALLOC_START, sizeof(unsigned char));
+    if(ret.digits == NULL) {
+        _FLAG_ = BAD_ALLOC;
+        return ret;
+    }
+
+    unsigned cap = ALLOC_START;
+
+    char digit;
+    while(fscanf(in ,"%c", &digit) == 1) {
+        if(isspace(digit)) {
+            break;
+        }
+
+        if(!isalnum(digit)) {
+            _FLAG_ = WRONG_INPUT;
+        }
+
+        if(ret.len == cap) {
+            cap *= ALLOC_EXPAND;
+            ret.digits = (char*)realloc(ret.digits, cap);
+            if(ret.digits == NULL) {
+                _FLAG_ = BAD_ALLOC;
+                return ret;
+            }
+        }
+
+        ret.digits[ret.len++] = char_to_int(digit);
+    }
+
+    scanf("%u", &ret.base);
+    return ret;
+}
+
+unsigned char int_to_char(unsigned char c) {
+    if(c >= 0 && c <= 9 ){
+        return c + '0';
+    }
+
+    else {
+        return c - 10 + 'A';
+    }
+}
+
+
+void print_number(struct number num) {
+    for(unsigned i = 0; i < num.len; ++i) {
+        printf("%c", int_to_char(num.digits[i]));
+    }
+}
+
+int is_zero(struct number number) {
+    for(int i = 0; i < number.len; ++i) {
+        if(number.digits[i] != 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+char divide(struct number number, unsigned new_base) {
+    int temp = 0;
+	for (unsigned i = 0; i < number.len; i++){
+		temp = temp * number.base + number.digits[i];
+		number.digits[i] = temp / new_base;
+		temp = temp % new_base;
+	}
+
+	return temp;
+}
+
+void reverse(struct number number) {
+    for(unsigned i = 0; i < number.len / 2; ++i) {
+        unsigned char tmp = number.digits[i];
+        number.digits[i] = number.digits[number.len - i - 1];
+        number.digits[number.len - i - 1] = tmp;
+    }
+}
+
+struct number change_base(struct number number, unsigned new_base) {
+    struct number ret = {NULL, 0, 0};
+
+    ret.digits = (char*)calloc(ALLOC_START, sizeof(unsigned char));
+    if(ret.digits == NULL) {
+        _FLAG_ = BAD_ALLOC;
+        return ret;
+    }
+
+    unsigned cap = ALLOC_START;
+
+    while(!is_zero(number)) {
+        if(ret.len == cap) {
+            cap *= ALLOC_EXPAND;
+            ret.digits = (char*)realloc(ret.digits, cap);
+            if(ret.digits == NULL) {
+                _FLAG_ = BAD_ALLOC;
+                return ret;
+            }
+        }
+
+        char digit = divide(number, new_base);
+        ret.digits[ret.len++] = digit;
+    }
+
+    reverse(ret);
+    ret.base = new_base;
+    return ret;
+}
+
+void delete_number(struct number number) {
+    if(number.digits != NULL) {
+        free(number.digits);
+    }
+}
+
+int main() {
+    struct number input_num = read_number(stdin);
+
+    unsigned new_base;
+    scanf("%u", &new_base);
+
+    input_num = change_base(input_num, new_base);
+    print_number(input_num);
+
+    delete_number(input_num);
+}
